165319b681e160e4072dbdcb3e494ecab5e8aa5b
Priyanshu Panwar <priyanshu009ch@gmail.com>
2020-05-06 13:07:40 +0530

    third commit

--- softkartpy/views.py
+++ softkartpy/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-	return HttpResponse("hello test case part -2 testing")
+	return HttpResponse("hello test case part -2 testing part -3 now")
